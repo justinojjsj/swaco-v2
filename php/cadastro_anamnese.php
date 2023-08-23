4752b4b3dcafb2e8263a0f5a77b2a6e49a18530c
Joel Justino <joelsaudesucesso@gmail.com>
2023-08-23 19:42:04 -0300

    Cadastro de CPF corrigido

--- php/cadastro_anamnese.php
+++ php/cadastro_anamnese.php
@@ -118,41 +118,11 @@
 			header("Location: index.php");
 		}
 
-		/*
-		
-		$sql = "INSERT INTO dados_anamnese VALUES";
-		$sql .= "(NULL,'$cpf','$problem','$visit','$medical','$allergies','$allergies_desc','$heart','$heart_desc','$benz','$benz_problem','$dipirona','$dip_problem','$pressure','$press_med','$press_medicine','$renal','$renal_problem','$diabete','$hepatite','$anest','$anest_problem','$protese','$marcap','$transf','$droga','$fuma','$gravida','$escovacao','$fio_dental',NOW(),NULL)";
-		
-		if ($conn->query($sql) === TRUE) {
-			$_SESSION['msg2'] = "sucesso";
-			header("Location: index.php");	
-		} else {
-			$_SESSION['msg2'] = "erro";
-		}
-		*/
 	}else{
 		//echo "INVALIDO";
 		$_SESSION['msg2'] = "cpf_invalido";
 		header("Location: anamnese-form2.php");
 	}	
 	$conn->close();
-
 	
 ?>
-
-
-<script>
-
-$(document).ready(function(){
-	$('a[data-confirm]').click(function(ev){
-		var href = $(this).attr('href');
-		if(!$('#confirm-delete').length){
-			$('body').append('<div class="modal fade" id="confirm-delete" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><div class="modal-header bg-danger text-white">EXCLUIR ITEM<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button></div><div class="modal-body">Tem certeza de que deseja excluir o item selecionado?</div><div class="modal-footer"><button type="button" class="btn btn-success" data-dismiss="modal">Cancelar</button><a class="btn btn-danger text-white" id="dataComfirmOK">Apagar</a></div></div></div></div>');
-		}
-		$('#dataComfirmOK').attr('href', href);
-		$('#confirm-delete').modal({show: true});
-		return false;
-	});
-});
-
-</script>

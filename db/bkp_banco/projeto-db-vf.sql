0a2f1d6b85d86ce52f44fbc03ed732c9d18eb305
justinojjsj <justinojjsj@gmail.com>
2023-11-07 10:22:35 -0300

    Versão final do BD

--- db/bkp_banco/projeto-db-vf.sql
+++ db/bkp_banco/projeto-db-vf.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: db
--- Tempo de geração: 21/05/2023 às 19:39
+-- Tempo de geração: 07/11/2023 às 13:17
 -- Versão do servidor: 10.11.2-MariaDB-1:10.11.2+maria~ubu2204
--- Versão do PHP: 8.1.17
+-- Versão do PHP: 8.1.15
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -67,11 +67,11 @@ CREATE TABLE `dados_anamnese` (
 --
 
 INSERT INTO `dados_anamnese` (`id_anamnese`, `CPF`, `problem`, `visit`, `medical`, `allergies`, `allergies_desc`, `heart`, `heart_desc`, `benz`, `benz_problem`, `dipirona`, `dip_problem`, `pressure`, `press_med`, `press_medicine`, `renal`, `renal_problem`, `diabete`, `hepatite`, `anest`, `anest_problem`, `protese`, `marcap`, `transf`, `droga`, `fuma`, `gravida`, `escovacao`, `fio_dental`, `criado`, `modificado`) VALUES
-(1, '463.426.098-02', 'ronco', '1 ano', 'nao', 'nao', NULL, 'nao', NULL, 'sim', 'nao', 'sim', 'nao', 'normal', 'sim', 'Losartana', 'nao', NULL, 'nao', 'nao', 'sim', 'nao', 'nao', 'nao', 'nao', 'nao', 'nao', 'nao', '3', '1', '2023-05-15', NULL),
-(2, '112.080.968-18', 'não há', '2 anos', 'nao', 'nao', NULL, 'nao', NULL, 'sim', 'nao', 'sim', 'nao', 'normal', 'nao', NULL, 'nao', NULL, 'nao', 'nao', 'sim', 'nao', 'nao', 'nao', 'nao', 'nao', 'nao', 'nao', '3', '1', '2023-05-16', NULL),
-(3, '836.655.988-26', 'ronco', '1 ano', 'nao', 'nao', NULL, 'sim', 'arritmia cardíaca', 'sim', 'nao', 'sim', 'nao', 'normal', 'nao', NULL, 'nao', NULL, 'nao', 'nao', 'sim', 'nao', 'nao', 'sim', 'nao', 'nao', 'nao', 'nao', '3', '1', '2023-05-17', NULL),
-(4, '695.167.228-81', 'não há', '2 anos', 'nao', 'nao', NULL, 'nao', NULL, 'sim', 'nao', 'sim', 'nao', 'normal', 'nao', NULL, 'nao', NULL, 'nao', 'nao', 'sim', 'nao', 'nao', 'nao', 'nao', 'nao', 'nao', 'nao', '3', '1', '2023-05-10', NULL),
-(5, '878.466.958-60', 'não há', '6 meses', 'nao', 'nao', NULL, 'nao', NULL, 'sim', 'nao', 'sim', 'nao', 'normal', 'nao', NULL, 'nao', NULL, 'nao', 'nao', 'sim', 'nao', 'nao', 'nao', 'nao', 'nao', 'nao', 'nao', '3', '1', '2023-05-17', NULL);
+(1, '46342609802', 'ronco', '1 ano', 'nao', 'nao', NULL, 'nao', NULL, 'sim', 'nao', 'sim', 'nao', 'normal', 'sim', 'Losartana', 'nao', NULL, 'nao', 'nao', 'sim', 'nao', 'nao', 'nao', 'nao', 'nao', 'nao', 'nao', '3', '1', '2023-11-01', NULL),
+(2, '11208096818', 'não há', '2 anos', 'nao', 'nao', NULL, 'nao', NULL, 'sim', 'nao', 'sim', 'nao', 'normal', 'nao', NULL, 'nao', NULL, 'nao', 'nao', 'sim', 'nao', 'nao', 'nao', 'nao', 'nao', 'nao', 'nao', '3', '1', '2023-11-02', NULL),
+(3, '83665598826', 'ronco', '1 ano', 'nao', 'nao', NULL, 'sim', 'arritmia cardíaca', 'sim', 'nao', 'sim', 'nao', 'normal', 'nao', NULL, 'nao', NULL, 'nao', 'nao', 'sim', 'nao', 'nao', 'sim', 'nao', 'nao', 'nao', 'nao', '3', '1', '2023-11-03', NULL),
+(4, '69516722881', 'não há', '2 anos', 'nao', 'nao', NULL, 'nao', NULL, 'sim', 'nao', 'sim', 'nao', 'normal', 'nao', NULL, 'nao', NULL, 'nao', 'nao', 'sim', 'nao', 'nao', 'nao', 'nao', 'nao', 'nao', 'nao', '3', '1', '2023-11-04', NULL),
+(5, '87846695860', 'não há', '6 meses', 'nao', 'nao', NULL, 'nao', NULL, 'sim', 'nao', 'sim', 'nao', 'normal', 'nao', NULL, 'nao', NULL, 'nao', 'nao', 'sim', 'nao', 'nao', 'nao', 'nao', 'nao', 'nao', 'nao', '3', '1', '2023-11-05', NULL);
 
 -- --------------------------------------------------------
 
@@ -92,7 +92,7 @@ CREATE TABLE `dados_paciente` (
   `convenio` varchar(20) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
   `CEP` varchar(20) DEFAULT NULL,
   `ad_number` varchar(10) DEFAULT NULL,
-  `street` varchar(200) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+  `street` varchar(40) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
   `district` varchar(40) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
   `city` varchar(40) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
   `state` varchar(10) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
@@ -105,11 +105,11 @@ CREATE TABLE `dados_paciente` (
 --
 
 INSERT INTO `dados_paciente` (`id_pac`, `CPF`, `nome`, `celular`, `idade`, `estado_civil`, `email`, `work`, `gender`, `convenio`, `CEP`, `ad_number`, `street`, `district`, `city`, `state`, `criado`, `modificado`) VALUES
-(1, '463.426.098-02', 'Priscila Stefany Amanda Mendes', '12 992251644', '53', 'casada', 'priscila_mendes@marcossousa.com', 'professora', 'feminino', 'particular', '12240530', '570', 'Avenida Campos Elíseos', 'Jardim Alvorada', 'São José dos Campos', 'SP', '2023-05-15', NULL),
-(2, '112.080.968-18', 'Giovanni Mário Duarte', '12 984007180', '23', 'solteiro', 'giovanni-duarte98@gruposandino.com.br', 'estudante', 'masculino', 'particular', '12230420', '573', 'Rua Virgem', 'Jardim Satélite', 'São José dos Campos', 'SP', '2023-05-16', NULL),
-(3, '836.655.988-26', 'Hugo Manoel da Paz', '12 986931110', '58', 'casado', 'hugo_manoel_dapaz@rubens.adm.br', 'empresário', 'masculino', 'particular', '12212810', '779', 'Praça César Traballi', 'Jardim Telespark', 'São José dos Campos', 'SP', '2023-05-17', NULL),
-(4, '695.167.228-81', 'Diogo Matheus Monteiro', '12 998927377', '39', 'solteiro', 'diogomatheusmonteiro@homtail.com', 'engenheiro', 'masculino', 'particular', '12224793', '445', 'Rua Marcílio Benedito Costa', 'Jardim Pararangaba', 'São José dos Campos', 'SP', '2023-05-10', NULL),
-(5, '878.466.958-60', 'Flávia Regina Esther Nunes', '12 999333279', '51', 'casada', 'flavia_regina_nunes@trevorh.com.br', 'dona de casa', 'feminino', 'particular', '12232882', '215', 'Rua Vinte Três', 'Conjunto Residencial Dom Pedro II', 'São José dos Campos', 'SP', '2023-05-17', NULL);
+(1, '46342609802', 'Priscila Stefany Amanda Mendes', '12 992251644', '53', 'casada', 'priscila_mendes@marcossousa.com', 'professora', 'feminino', 'particular', '12240530', '570', 'Avenida Campos Elíseos', 'Jardim Alvorada', 'São José dos Campos', 'SP', '2023-11-01', NULL),
+(2, '11208096818', 'Giovanni Mário Duarte', '12 984007180', '23', 'solteiro', 'giovanni-duarte98@gruposandino.com.br', 'estudante', 'masculino', 'particular', '12230420', '573', 'Rua Virgem', 'Jardim Satélite', 'São José dos Campos', 'SP', '2023-11-02', NULL),
+(3, '83665598826', 'Hugo Manoel da Paz', '12 986931110', '58', 'casado', 'hugo_manoel_dapaz@rubens.adm.br', 'empresário', 'masculino', 'particular', '12212810', '779', 'Praça César Traballi', 'Jardim Telespark', 'São José dos Campos', 'SP', '2023-11-03', NULL),
+(4, '69516722881', 'Diogo Matheus Monteiro', '12 998927377', '39', 'solteiro', 'diogomatheusmonteiro@homtail.com', 'engenheiro', 'masculino', 'particular', '12224793', '445', 'Rua Marcílio Benedito Costa', 'Jardim Pararangaba', 'São José dos Campos', 'SP', '2023-11-04', NULL),
+(5, '87846695860', 'Flávia Regina Esther Nunes', '12 999333279', '51', 'casada', 'flavia_regina_nunes@trevorh.com.br', 'dona de casa', 'feminino', 'particular', '12232882', '215', 'Rua Vinte Três', 'Conjunto Residencial Dom Pedro II', 'São José dos Campos', 'SP', '2023-11-05', NULL);
 
 -- --------------------------------------------------------
 
@@ -120,22 +120,23 @@ INSERT INTO `dados_paciente` (`id_pac`, `CPF`, `nome`, `celular`, `idade`, `esta
 CREATE TABLE `events` (
   `id` int(11) NOT NULL,
   `title` varchar(220) NOT NULL,
-  `color` varchar(10) NOT NULL,
+  `color` varchar(30) DEFAULT NULL,
   `start` datetime NOT NULL,
   `end` datetime NOT NULL,
-  `url` varchar(220) DEFAULT NULL
+  `url` varchar(220) DEFAULT NULL,
+  `modificado` varchar(220) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Despejando dados para a tabela `events`
 --
 
-INSERT INTO `events` (`id`, `title`, `color`, `start`, `end`, `url`) VALUES
-(1, 'Priscila Stefany Amanda Mendes', '#deaac1', '2023-05-15 14:00:00', '2023-05-15 16:00:00', 'necessita de AIO, aparelho intraoral para ronco'),
-(2, 'Giovanni Mário Duarte', '#d9ead3', '2023-05-16 16:00:00', '2023-05-16 17:00:00', 'iniciado clareamento dental caseiro'),
-(3, 'Hugo Manoel da Paz', '#c0a78b', '2023-05-17 16:00:00', '2023-05-17 18:00:00', 'paciente classe 2, iniciado tratamento de reabilitação oral na arcada superior'),
-(4, 'Diogo Matheus Monteiro', '#a2eaca', '2023-05-10 13:30:00', '2023-05-10 15:00:00', 'efetuadas 2 restaurações na oclusal do 46 e na oclusal do 47'),
-(5, 'Flávia Regina Esther Nunes', '#ecbee9', '2023-05-17 14:30:00', '2023-05-17 16:00:00', 'efetuada limpeza e iniciado clareamento dental caseiro');
+INSERT INTO `events` (`id`, `title`, `color`, `start`, `end`, `url`, `modificado`) VALUES
+(1, 'Priscila Stefany Amanda Mendes', '46342609802', '2023-11-06 14:00:00', '2023-05-15 16:00:00', 'necessita de AIO, aparelho intraoral para ronco', NULL),
+(2, 'Giovanni Mário Duarte', '11208096818', '2023-11-08 16:00:00', '2023-11-08 17:00:00', 'iniciado clareamento dental caseiro', NULL),
+(3, 'Hugo Manoel da Paz', '83665598826', '2023-11-14 16:00:00', '2023-11-14 18:00:00', 'paciente classe 2, iniciado tratamento de reabilitação oral na arcada superior', NULL),
+(4, 'Diogo Matheus Monteiro', '69516722881', '2023-11-15 13:30:00', '2023-11-15 15:00:00', 'efetuadas 2 restaurações na oclusal do 46 e na oclusal do 47', NULL),
+(5, 'Flávia Regina Esther Nunes', '87846695860', '2023-11-22 14:30:00', '2023-11-22 16:00:00', 'efetuada limpeza e iniciado clareamento dental caseiro', NULL);
 
 --
 -- Índices para tabelas despejadas
